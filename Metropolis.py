0f05f61b616737ccd7a0779833a285ae933bec58
aravind <akr947@umd.edu>
2018-12-04 17:53:02 -0500

    refactored the program and fixed energy divergence issue

--- Metropolis.py
+++ Metropolis.py
@@ -1,117 +1,110 @@
-# Metropolis simulation for Ising model
+#!/usr/bin/env python
 
+# Metropolis simulation for Ising model
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as clr
 import seaborn as sns; sns.set()
 
-#constant values
-J = -1
-T = 1
-#k = 1.38 * 10 ** (-23)
-k=1
+# constant values
+# TODO: set up proper constants so that the dE is in meaningful units
+J = -1 # energy unit
+T = 1 # temp unit
+k = 1 # boltzmann factor
 
-def generate_matrix(size):
-    spins = np.zeros((size, size))
-    for i in range(0, size):
-        for j in range(0, size):
+# colormap for seaborn
+colors = ((0.0,0.0,0.0,1.0), (0.6,0.6,0.6,1.0)) # black and grey for spin up/down
+cm = clr.LinearSegmentedColormap.from_list('Custom', colors, len(colors))
+
+# returns matrix of spins, avg magnetization, initial interaction energy
+def generate_matrix(l,w):
+    spins = np.zeros((w, l))
+    e = 0
+    magnetization = 0
+    for i in range(0, w):
+        for j in range(0, l):
             spins[i, j] = np.random.choice([-1, 1])
-    return spins
+            magnetization += spins[i,j]
+            e += spins[i, j] * (spins[i, (j+1)%l] + spins[(i+1)%w, j])
+    return (spins, magnetization/(l*w), J*e)
 
-def magnetization(spins, length, height):
-    sum = 0;
-    N = length * height;
-    for i in range(0, length):
-        for j in range(0, height):
-            sum += spins[i,j]
-    return sum/N
-
-def deltaE(spins, i, j):
-    max = spins[0].size-1
+def deltaE(spins, i, j, l, w):
     spin = -1*spins[i,j]
 
-    #Assumes boundaries loop over
-    #special cases at i,j = 0 and i,j = max
-    L = (j-1)%(max-1)
-    R = (j+1)%(max-1)
-    U = (i+1)%(max-1)
-    D = (i-1)%(max-1)
+    # find positions of adjacent points, assume boundaries loop over
+    L = (i-1)%(w)
+    R = (i+1)%(w)
+    U = (j+1)%(l)
+    D = (j-1)%(l)
 
-    #Add the new spin interactions together to find
-    #change in energy
-    #the interactions will be with the
-    #left, right, up, and down entries
-    return 2*J*spin*(spins[i,L]+spins[i,R]+spins[U,j]+spins[D,j])
+    # Add the new spin interactions with adj points to find change in energy
+    return 2*J*spin*(spins[L,j] + spins[R,j] + spins[i,U] + spins[i,D])
 
 
-#picks a random entry and follows the procedure
-#that decides whether or not it should switch signs
-def swap_iteration(spins):
-    #choose a random entry to consider
-    i = np.random.randint(0,spins[0].size)
-    j = np.random.randint(0, spins[0].size)
+# picks a random entry and follows the procedure to decide whether to  switch signs
+def swap_iteration(spins, energy, l, w):
+    # choose a random entry to consider
+    i = np.random.randint(0, w)
+    j = np.random.randint(0, l)
 
-    #find change in energy if we were to swap it
-    dE = deltaE(spins, i, j)
+    # find change in energy if we were to swap it
+    dE = deltaE(spins, i, j, l, w)
 
-    #if energy decreases, swap it
+    # if energy decreases, swap it
     if( dE < 0 ):
         spins[i, j] *= -1
+        energy = energy + dE
 
     else:
         # pick random num between 0 and 1
         r = np.random.rand()
-        #Find the probability of swapping
+        # Find the probability of swapping
         P = np.exp(-dE/(k*T))
 
-        #if r is within that probability, swap
+        # if r is within that probability, swap
         if( r <= P ):
             spins[i, j] *= -1
+            energy = energy + dE
             #print("swapped")
-    return spins
-
-#calculates the energy from all of the spin interactions
-def get_energy(spins):
-    e = 0
-    max = spins[0].size-1
-    #To find energy, add up the interaction energies
-    for i in range(0, spins[0].size):
-        for j in range(0, spins[0].size):
-            e +=spins[i, j] * (spins[i, (j+1)%max] +                               spins[(i+1)%max, j])
-    return J*e
+    return (spins, energy)
 
 def run(dims, iterations):
-    #get a matrix filled with random 1's and -1's
-    spins = generate_matrix(dims);
+    # Generate matrix, get initial conditions
+    (spins, mag, energy) = generate_matrix(dims,dims);
 
     plt.figure(1)
-    plt.title("Initial spin distribution")
-    sns.heatmap(spins)
+    plt.title("Initial Spin Distribution")
+    plt.axis('off')
+    p0 = sns.heatmap(spins, cmap=cm)
+    cbar= p0.collections[0].colorbar
+    cbar.set_ticks([-1,1])
+    cbar.set_ticklabels(["spin up", "spin down"])
 
-    X = []
-    E = []
+    X = [] # iteration number for energy vs iteration plot
+    E = [] # total energy per iteration
     M = []
     avgE = []
 
-    e_tot = 0
-
     for i in range(0, iterations):
-        #add the iteration num to our x values for the plot
         X.append(i)
-        #run the swap process on a random entry
-        spins = swap_iteration(spins)
-        #find the total energy
-        e_tot = get_energy(spins)
-        E.append(e_tot)
-        #M.append(magnetization(spins, spins[0].size, spins[0].size))
+        (spins, energy) = swap_iteration(spins, energy, dims, dims) # swap process for random entry
+        E.append(energy)
+        #M.append(magnetization(spins, spins[0].size, spins[0].size)) # TODO: add this as a piece of the swap process like energy
 
-    #Plot energy vs number of iterations
-    plt.figure(3)
-    plt.title("Energy vs iterations")
+    # Plot final characteristics
+    p3 = plt.figure(3)
+    plt.title("Change in Energy vs Iterations")
+    plt.xlabel("iteration")
+    plt.ylabel("dE")
     plt.plot(X, E)
-    #plt.show()
     plt.figure(2)
-    plt.title("Spin dist. after performing all iterations")
-    sns.heatmap(spins)
 
+    plt.title("Spin Distribution After " + str(iterations)  +  " Iterations")
+    p2 = sns.heatmap(spins, cmap=cm)
+    plt.axis('off')
+    cbar= p2.collections[0].colorbar
+    cbar.set_ticks([-1,1])
+    cbar.set_ticklabels(["spin up", "spin down"])
+    plt.show()
 
-run(50, 10000)
+run(100, 100000)
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Introduction
+This is a program for that uses the Metropolis Algorithm to determine the change of spin states in a crystal model
