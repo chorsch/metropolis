6ccbccc13babc7fa6cd95e4d0946ca114e36782c
aravind <akr947@umd.edu>
2018-12-06 16:44:36 -0500

    code no longer has millions of function calls, further optimized

--- Metropolis.py
+++ Metropolis.py
@@ -2,6 +2,7 @@
 
 # Metropolis simulation for Ising model
 import numpy as np
+from scipy.ndimage import convolve
 import matplotlib.pyplot as plt
 import matplotlib.colors as clr
 import seaborn as sns; sns.set()
@@ -9,26 +10,19 @@ import seaborn as sns; sns.set()
 # constant values
 # TODO: set up proper constants so that the dE is in meaningful units
 J = -1 # energy unit
-T = 1 # temp unit
 k = 0.25 # boltzmann factor
 
 # colormap for seaborn
 colors = ((0.0,0.0,0.0,1.0), (0.6,0.6,0.6,1.0)) # black and grey for spin up/down
 cm = clr.LinearSegmentedColormap.from_list('Custom', colors, len(colors))
 
-S = []
-
 # returns matrix of spins, avg magnetization, initial interaction energy
 def generate_matrix(l,w):
-    spins = np.zeros((w, l))
-    e = 0
-    magnetization = 0
-    for i in range(0, w):
-        for j in range(0, l):
-            spins[i, j] = np.random.choice([-1, 1])
-            magnetization += spins[i,j]
-            e += spins[i, j] * (spins[i, (j+1)%l] + spins[(i+1)%w, j])
-    return (spins, magnetization/(l*w), J*e)
+    spins = np.random.choice((-1,1), (l,w))
+    kernel = np.array([[0,1,0], [1,0,1], [0,1,0]])
+    energy = J*np.sum(convolve(spins, kernel, mode='wrap')*spins)
+    magnetization = np.average(spins)
+    return (spins, energy, magnetization)
 
 def deltaE(spins, i, j, l, w):
     spin = -1*spins[i,j]
@@ -44,42 +38,46 @@ def deltaE(spins, i, j, l, w):
 
 
 # picks a random entry and follows the procedure to decide whether to  switch signs
-def swap_iteration(spins, energy, magnetization, l, w):
-    # choose a random entry to consider
-    i = np.random.randint(0, w)
-    j = np.random.randint(0, l)
+def metropolis(spins, energy, magnetization, l, w, N, T, X, E, M, S):
+    switches = 0
+    for n in range(0,N):
+        X.append(n)
+        # choose a random entry to consider
+        i = np.random.randint(0, w)
+        j = np.random.randint(0, l)
 
-    global switches, S
 
-    # find change in energy if we were to swap it
-    dE = deltaE(spins, i, j, l, w)
+        # find change in energy if we were to swap it
+        dE = deltaE(spins, i, j, l, w)
 
-    # if energy decreases, swap it
-    if( dE < 0 ):
-        spins[i, j] *= -1
-        energy = energy + dE
-        switches += 1
-        magnetization += 2*spins[i,j]/(l*w)
-
-    else:
-        # pick random num between 0 and 1
-        r = np.random.rand()
-        # Find the probability of swapping
-        P = np.exp(-dE/(k*T))
-
-        # if r is within that probability, swap
-        if( r <= P ):
+        # if energy decreases, swap it
+        if( dE < 0 ):
             spins[i, j] *= -1
             energy = energy + dE
             switches += 1
             magnetization += 2*spins[i,j]/(l*w)
-            #print("swapped")
-    S.append(switches)
-    return (spins, energy, magnetization)
+
+        else:
+            # pick random num between 0 and 1
+            r = np.random.rand()
+            # Find the probability of swapping
+            P = np.exp(-dE/(k*T))
+
+            # if r is within that probability, swap
+            if( r <= P ):
+                spins[i, j] *= -1
+                energy = energy + dE
+                switches += 1
+                magnetization += 2*spins[i,j]/(l*w)
+                #print("swapped")
+        S.append(switches)
+        M.append(magnetization)
+        E.append(energy)
+    return (X, E, M, S)
 
 def run(dims, iterations):
     # Generate matrix, get initial conditions
-    (spins, mag, energy) = generate_matrix(dims,dims);
+    (spins, energy, mag) = generate_matrix(dims,dims);
 
     plt.figure(1)
     plt.title("Initial Spin Distribution")
@@ -89,16 +87,14 @@ def run(dims, iterations):
     cbar.set_ticks([-1,1])
     cbar.set_ticklabels(["spin down", "spin up"])
 
+    T = 1 # temperature
+
     X = [] # iteration number for energy vs iteration plot
     E = [] # total energy per iteration
-    M = []
-    avgE = []
+    M = [] # mean magnetization
+    S = [] # total number of swaps
 
-    for i in range(0, iterations):
-        X.append(i)
-        (spins, energy, mag) = swap_iteration(spins, energy, mag, dims, dims) # swap process for random entry
-        E.append(energy)
-        M.append(mag)
+    (X, E, M, S) = metropolis(spins, energy, mag, dims, dims, iterations, T, X, E, M, S) # swap process for random entry
 
     # Plot final characteristics
     p3 = plt.figure(3)
@@ -129,5 +125,6 @@ def run(dims, iterations):
 
     plt.show()
 
-switches = 0
+    return (E[-1], M[-1]) # tuple of energy, mean magnetization
+
 run(50, 3000000)
